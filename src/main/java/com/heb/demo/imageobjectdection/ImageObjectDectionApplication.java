5f61db11e40a98b6b7f403d6d619ca0984bbb355
William Barfield <willbarfield@hotmail.com>
2022-10-05 18:52:16 -0500

    spring boot init code

---
+++ src/main/java/com/heb/demo/imageobjectdection/ImageObjectDectionApplication.java
@@ -0,0 +1,13 @@
+package com.heb.demo.imageobjectdection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageObjectDectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageObjectDectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heb/demo/imageobjectdection/ImageObjectDectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heb.demo.imageobjectdection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageObjectDectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
